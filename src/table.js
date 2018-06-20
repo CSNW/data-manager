9f87db455c7e8fd76c6bb2c08e83a03ab32a95f5
Tim Hall <tim.hall.engr@gmail.com>
2018-06-20 11:25:22 -0400

    Add usage to table

--- src/table.js
+++ src/table.js
@@ -1,3 +1,30 @@
+/**
+ * Load csv data table for given path.
+ *
+ * - On first load, it will fetch, convert, and cache csv
+ * - On subsequent loads, will load directly from cache
+ * - `convert` performs filter and map, return `null` or `undefined` to filter row
+ *
+ * @example
+ * ```js
+ * import { table } from 'data-manager';
+ *
+ * const population = table('population.csv', row => {
+ *   // return null or undefined to filter row
+ *   if (row.invalid) return;
+ *
+ *   return {
+ *     year: new Date(+row.year, 0, 1),
+ *     population: +row.population
+ *   };
+ * });
+ *
+ * // -> use `population` in store.query(...)
+ * ```
+ * @param {string} path
+ * @param {function} convert map and filter rows
+ * @returns {function} operation
+ */
 export default function table(path, convert) {
   return (_, store) => {
     return store.load(path, convert).then(values => [{ values }]);
